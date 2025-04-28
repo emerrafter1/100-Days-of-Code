733122057f738ce4783e7f74aed4b03fbc6d9b8e
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-28 11:57:30 +0100

    Completed Band Generator

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-print("My name is" + " " + "Angela")
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name is: " + city + " " + pet_name)
