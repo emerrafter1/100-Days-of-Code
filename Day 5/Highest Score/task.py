33dcadbd9170b26fd48916bc3eeebc54a3ad5e43
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-30 16:31:26 +0100

    completed day 5

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,21 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+
+scores = [8, 65, 89, 86, 55, 91, 64, 89]
+
+max_score = scores[0]
+
+for score in scores:
+    if score > max_score:
+        max_score=score
+
+max_student_score = student_scores[0]
+
+for score in student_scores:
+    if score > max_student_score:
+        max_student_score=score
+
+
+print(max_score)
+print(max_student_score)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,4 @@
+import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -8,3 +9,18 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
+password = []
+
+for num in range(0, nr_letters):
+    password.append(random.choice(letters))
+
+for num in range(0, nr_symbols):
+    password.append(random.choice(symbols))
+
+for num in range(0, nr_numbers):
+    password.append(random.choice(numbers))
+
+random.shuffle(password)
+strongPassword = "".join(password)
+
+print(strongPassword)
